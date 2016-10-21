974517b69be72517647da71f4f09bc5d6bd83585
Gabbo <lunagabrielricardo@gmail.com>
2016-10-21 01:49:30 -0300

    testeando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,13 +60,15 @@
                 <ul class="nav navbar-nav navbar-right">
 
                     <!-- Authentication Links -->
-                    <li>
-                    <a href="{{url('/carrito')}}">
-                        Mi carrito
-                        <span class="circle-shopping-cart">
-                            {{$productsCount}}
-                        </span>
-                    </a>
+                    <li><a href="{{url('/carrito')}}" style="padding-right: 7px;">Mi carrito</a></li>
+                    <li class="icon-cart"> 
+                        <a href="{{url('/carrito')}}" style="padding: 14px;padding-left: 0; padding-right: 20px;">
+                            <svg width="100%" height="100%" viewBox="8.3 0 100.5 126.1" preserveAspectRatio="xMinYMax meet" ng-switch-when="1" style="transform-origin: 50%">
+                                <path d="M99.8 28.4c0-1.2-0.9-2-2.1-2h-15c0 3.2 0 7.6 0 8.2 0 1.5-1.2 2.6-2.6 2.9 -1.5 0.3-2.9-0.9-3.2-2.3 0-0.3 0-0.3 0-0.6 0-0.9 0-4.7 0-8.2H40.1c0 3.2 0 7.3 0 8.2 0 1.5-1.2 2.9-2.6 2.9 -1.5 0-2.9-0.9-3.2-2.3 0-0.3 0-0.3 0-0.6 0-0.6 0-5 0-8.2h-15c-1.2 0-2 0.9-2 2L8.3 124c0 1.2 0.9 2.1 2.1 2.1h96.3c1.2 0 2.1-0.9 2.1-2.1L99.8 28.4z" style="fill: rgba(255,255,255,.84);"></path>
+                                <path d="M59.1 5.9c-2.9 0-2 0-2.9 0 -2 0-4.4 0.6-6.4 1.5 -3.2 1.5-5.9 4.1-7.6 7.3 -0.9 1.8-1.5 3.5-1.8 5.6 0 0.9-0.3 1.5-0.3 2.3 0 1.2 0 2.1 0 3.2 0 1.5-1.2 2.9-2.6 2.9 -1.5 0-2.9-0.9-3.2-2.3 0-0.3 0-0.3 0-0.6 0-1.2 0-2.3 0-3.5 0-3.2 0.9-6.4 2-9.4 1.2-2.3 2.6-4.7 4.7-6.4 3.2-2.9 6.7-5 11.1-5.9C53.5 0.3 55 0 56.7 0c1.5 0 2.9 0 4.4 0 2.9 0 5.6 0.6 7.9 1.8 2.6 1.2 5 2.6 6.7 4.4 3.2 3.2 5.3 6.7 6.4 11.1 0.3 1.5 0.6 3.2 0.6 4.7 0 1.2 0 2.3 0 3.2 0 1.5-1.2 2.6-2.6 2.9s-2.9-0.9-3.2-2.3c0-0.3 0-0.3 0-0.6 0-1.2 0-2.6 0-3.8 0-2.3-0.6-4.4-1.8-6.4 -1.5-3.2-4.1-5.9-7.3-7.3 -1.8-0.9-3.5-1.8-5.9-1.8C61.1 5.9 59.1 5.9 59.1 5.9L59.1 5.9z" style="fill: rgba(255,255,255,.84);"></path>
+                                <text x="58.5" y="77" dy=".35em" text-anchor="middle" style="fill: #000000;font-size: 3.5em;">{{$productsCount}}</text>
+                            </svg>
+                        </a>
                     </li>
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
